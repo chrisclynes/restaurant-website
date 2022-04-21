f5094045cd9d06ef43791252981e85998a2cf82d
Chris Clynes <chrisclynes@live.com>
2022-04-20 22:15:26 -0400

    first commit, core build

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/md';
+import { MdOutlineRestuarantMenu } from 'react-icons/md';
+
+import images from '../../constants/images';
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="app__navbar">
+    Navbar
+  </nav>
+);
+
+export default Navbar;
---
+++ src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const wines = [
+  {
+    title: 'Sangiovese',
+    price: '$56',
+    tags: 'IT | Bottle',
+  },
+  {
+    title: 'Barbera',
+    price: '$59',
+    tags: 'IT | Bottle',
+  },
+  {
+    title: "Nero d'Avola",
+    price: '$54',
+    tags: 'IT | 750 ml',
+  },
+  {
+    title: 'Dolcetto',
+    price: '$67',
+    tags: 'IT | Bottle',
+  },
+  {
+    title: 'Nebbiolo',
+    price: '$89',
+    tags: 'IT | Bottle',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Martini',
+    price: '$22',
+    tags: 'Vodka | Gin',
+  },
+  {
+    title: "Manhattan",
+    price: '$21',
+    tags: 'Canadian Whiskey | Sweet Red | Bitters',
+  },
+  {
+    title: 'Siesta Cocktail',
+    price: '$17',
+    tags: 'Tequila | Grapefruit Juice | Simple Syrup | Campari',
+  },
+  {
+    title: 'Old Fashioned',
+    price: '$28',
+    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+  },
+  {
+    title: 'Boulevardier',
+    price: '$26',
+    tags: 'Sweet Vermouth | bourbon  | Campari | Maraschino Cherry',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Bib Gourmond',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Rising Star',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'AA Hospitality',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Outstanding Chef',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+];
+
+export default { wines, cocktails, awards };
