b9158c4c5ca1c5b92cc5ba901f7b84fb294f0608
Chris Clynes <chrisclynes@live.com>
2022-04-21 23:39:19 -0400

    nav bar basics built with mobile reponse

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/md';
-import { MdOutlineRestuarantMenu } from 'react-icons/md';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 
 import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
   <nav className="app__navbar">
-    Navbar
+    <div className="app__navbar-logo">
+      <img src={images.flavios} alt="restuarant logo" />
+    </div>
+    <div className="nav_items-container">
+      <div className="nav_items">
+        <ul className="app__navbar-links">
+          <li className="nav_item p__opensans"><a href="#home">Home</a></li>
+          <li className="nav_item p__opensans"><a href="#about">About</a></li>
+          <li className="nav_item p__opensans"><a href="#menu">Menu</a></li>
+          <li className="nav_item p__opensans"><a href="#awards">Awards</a></li>
+          <li className="nav_item p__opensans"><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+      <div className="nav_items-right">
+        <ul className="app__navbar-loginbook">
+          <li className="nav_item p__opensans"><a href="/">Book Table</a></li>
+          <li className="nav_item p__opensans"><a href="#login">Sign In</a></li>
+        </ul>
+      </div>
+    </div>
+        <div className="app__navbar-smallscreen">
+          <GiHamburgerMenu size={21} color="#fff" onClick={() => {}}/>
+        </div>
+        <div className="app__navbar-smallscreen-container">
+            <div className="app__navbar-smscreen_overlay flex__center slide-left">
+            <button className="crossBtn p__opensans" onClick={() => {}} >x</button>
+              <ul className="app__navbar-smallscreen-links">
+                <li className="sidebar_item p__opensans"><a href="#home">Home</a></li>
+                <li className="sidebar_item p__opensans"><a href="#about">About</a></li>
+                <li className="sidebar_item p__opensans"><a href="#menu">Menu</a></li>
+                <li className="sidebar_item p__opensans"><a href="#awards">Awards</a></li>
+                <li className="sidebar_item p__opensans"><a href="#contact">Contact</a></li>
+                <li className="sidebar_item p__opensans"><a href="/">Book Table</a></li>
+                <li className="sidebar_item p__opensans"><a href="#login">Sign In</a></li>
+              </ul>
+            </div>
+          </div> 
+          
+          
+      
+    
+    
   </nav>
 );
 
