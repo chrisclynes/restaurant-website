1fc99eac5608e783fca08f484295f6fdf61bebfd
Chris Clynes <chrisclynes@live.com>
2022-04-22 23:45:04 -0400

    fixed issues with nav on all platforms, building header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
   <nav className="app__navbar">
     <div className="app__navbar-logo">
       <img src={images.flavios} alt="restuarant logo" />
@@ -27,29 +30,30 @@ const Navbar = () => (
         </ul>
       </div>
     </div>
+        {!toggleMenu && (
         <div className="app__navbar-smallscreen">
-          <GiHamburgerMenu size={21} color="#fff" onClick={() => {}}/>
+          <GiHamburgerMenu size={21} color="#fff" onClick={() => setToggleMenu(true)}/>
         </div>
+        )}
+        {toggleMenu && (
         <div className="app__navbar-smallscreen-container">
             <div className="app__navbar-smscreen_overlay flex__center slide-left">
-            <button className="crossBtn p__opensans" onClick={() => {}} >x</button>
-              <ul className="app__navbar-smallscreen-links">
-                <li className="sidebar_item p__opensans"><a href="#home">Home</a></li>
-                <li className="sidebar_item p__opensans"><a href="#about">About</a></li>
-                <li className="sidebar_item p__opensans"><a href="#menu">Menu</a></li>
-                <li className="sidebar_item p__opensans"><a href="#awards">Awards</a></li>
-                <li className="sidebar_item p__opensans"><a href="#contact">Contact</a></li>
-                <li className="sidebar_item p__opensans"><a href="/">Book Table</a></li>
-                <li className="sidebar_item p__opensans"><a href="#login">Sign In</a></li>
-              </ul>
+            <button className="crossBtn p__opensans" onClick={() => setToggleMenu(false)} >x</button>
+              <div className="app__navbar-smallscreen-links-container">
+                <ul className="app__navbar-smallscreen-links">
+                  <li className="sidebar_item p__opensans"><a href="#home">Home</a></li>
+                  <li className="sidebar_item p__opensans"><a href="#about">About</a></li>
+                  <li className="sidebar_item p__opensans"><a href="#menu">Menu</a></li>
+                  <li className="sidebar_item p__opensans"><a href="#awards">Awards</a></li>
+                  <li className="sidebar_item p__opensans"><a href="#contact">Contact</a></li>
+                  <li className="sidebar_item p__opensans"><a href="/">Book Table</a></li>
+                  <li className="sidebar_item p__opensans"><a href="#login">Sign In</a></li>
+                </ul>
+              </div>
             </div>
-          </div> 
-          
-          
-      
-    
-    
+          </div>
+         )}
   </nav>
-);
+  )};
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Find your flavor" />
+      <h1 className="app__header-h1">The True Taste of Italy</h1>
+    </div>
+    <div className="app__wrapper_img">
+
+    </div>
   </div>
+  
+  
 );
 
 export default Header;
