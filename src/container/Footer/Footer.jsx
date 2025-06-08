89c4998dbbd30e46c37b1629aa5989e4e2a17df8
chrisclynes <chrisclynes@live.com>
2025-06-08 16:00:45 -0500

    Footer updates

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -5,24 +5,30 @@ import { images } from '../../constants';
 import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
-const Footer = () => (
-  <div className="app__footer section__padding">
-    <FooterOverlay />
-    <Newsletter />
-    <div className="app__footer-links" style={{zIndex: "1"}}>
-      <div className="app__footer-links_icons">
-        <FiFacebook size={22} />
-        <FiTwitter size={22} />
-        <FiInstagram size={22} />
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="app__footer section__padding">
+      <FooterOverlay />
+      <Newsletter />
+
+      <div className="app__footer-links" style={{ zIndex: "1" }}>
+        <div className="app__footer-links_icons">
+          <FiFacebook size={22} />
+          <FiTwitter size={22} />
+          <FiInstagram size={22} />
+        </div>
+        <div className="app__footer-links_logo">
+          <img src={images.flavios} alt="logo" />
+        </div>
       </div>
-      <div className="app__footer-links_logo">
-        <img src={images.flavios} alt="logo" />
+
+      <div className="footer__copyright" style={{ zIndex: "1" }}>
+        <p className="p__opensans">© {currentYear} Flavio's. All Rights Reserved.</p>
       </div>
     </div>
-    <div className="footer__copyright" style={{zIndex: "1"}}>
-      <p className="p__opensans">2022 Flavio's. All Rights Reserved.</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Footer;
